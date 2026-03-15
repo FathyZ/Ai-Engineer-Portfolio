d189bf719df7d42105ab20447a8b5cbaa9142519
FathyZ <hammouda.fathy@gmail.com>
2026-03-16 00:28:11 +0200

    removed unused router import - for now -

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import NeuralMesh from "../components/NeuralMesh";
 import ProjectCard from "../components/ProjectCard";
 import { PROJECTS } from "../data.ts";
